ce7566c46c1dc29699b89573bdff1c20debd08e1
Alejandro Medina <axedro@yahoo.es>
2025-08-01 15:45:25 +0100

    fix: crear configuración ESLint independiente para apps/web
    - Configuración ESLint completa y autónoma para el paquete web
    - No depende de la configuración del paquete raíz
    - Incluye todas las configuraciones necesarias para React y TypeScript
    - Evita problemas de dependencias compartidas

--- flowcraft/apps/web/.eslintrc.cjs
+++ flowcraft/apps/web/.eslintrc.cjs
@@ -1,12 +1,27 @@
 module.exports = {
-  extends: ['../../.eslintrc.js'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: ['react', 'react-hooks'],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
   rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
   },
@@ -15,4 +30,5 @@ module.exports = {
       version: 'detect',
     },
   },
+  ignorePatterns: ['dist/', 'node_modules/'],
 }; 
