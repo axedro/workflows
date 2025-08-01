9b9ce91f5c70b404d4fe585ea01490017454f71e
Alejandro Medina <axedro@yahoo.es>
2025-08-01 16:05:01 +0100

    fix: corregir configuración de ESLint para React
    - Añadir 'react/jsx-uses-react': 'off' para soportar el nuevo JSX transform.
    - Corregir la estructura del archivo .eslintrc.js.
    - Asegurar que las reglas de React se apliquen correctamente.

--- flowcraft/.eslintrc.js
+++ flowcraft/.eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
       plugins: ['react', 'react-hooks'],
       rules: {
         'react/react-in-jsx-scope': 'off',
+        'react/jsx-uses-react': 'off',
         'react/prop-types': 'off',
       },
       settings: {
